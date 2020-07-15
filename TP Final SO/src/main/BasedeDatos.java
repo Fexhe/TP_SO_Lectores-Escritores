a252fbe1a4a298d7b80c0a02c7558ed92c5edc7c
krnnr <krnnr@LAPTOP-2CEJJ64B>
2020-07-14 22:46:44 -0300

    ordenamiento de arreglos

--- TP Final SO/src/main/BasedeDatos.java
+++ TP Final SO/src/main/BasedeDatos.java
@@ -53,6 +53,22 @@ public class BasedeDatos {
 		}
 		
 
+		private static void burbuja(int[] arreglo) {
+			for (int x = 0; x < arreglo.length; x++) {
+			// Aquí "y" se detiene antes de llegar
+			// a length - 1 porque dentro del for, accedemos
+			// al siguiente elemento con el índice actual + 1
+				for (int y = 0; y < arreglo.length - 1; y++) {
+					int elementoActual = arreglo[y],
+							elementoSiguiente = arreglo[y + 1];
+					if (elementoActual > elementoSiguiente) {
+						// Intercambiar
+						arreglo[y] = elementoSiguiente;
+						arreglo[y + 1] = elementoActual;
+					}
+				}
+			}
+		}
 
 	}
 }
